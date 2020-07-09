283eb99fd7aa665f319f5a590230e1733bff09ec
beklenishchevS <50109736+beklenishchevS@users.noreply.github.com>
2020-07-09 18:19:45 +0500

    init commit to TargetGames work 2

---
+++ MyList/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myList = new MyList<int>();
+            myList.Add(1);
+            myList.Add(2);
+            myList.Add(3);
+            myList.Add(4);
+            foreach(var e in myList)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
